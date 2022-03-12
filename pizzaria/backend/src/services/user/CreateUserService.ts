8a4794ae8c87750115ab099f31bdb8dc62d84677
Gustavo <gustavofloripa98@gmail.com>
2022-03-12 20:31:34 -0300

    first commit

---
+++ pizzaria/backend/src/services/user/CreateUserService.ts
@@ -0,0 +1,15 @@
+
+interface UserRequest{
+    name: String,
+    email: String,
+    password: String
+}
+
+class CreateUserService {
+    async execute({name, email, password}: UserRequest){
+
+        return {name, email, password}
+    }
+}
+
+export { CreateUserService };
