9692f20c32b0616c7f520d2d9cddb35df0bb2261
Gustavo <gustavofloripa98@gmail.com>
2022-03-13 20:34:00 -0300

    filtrando produtos de uma categoria

--- pizzaria/backend/src/routes.ts
+++ pizzaria/backend/src/routes.ts
@@ -7,6 +7,7 @@ import { DetailUserController } from './controllers/user/DetailUserController';
 import { CreateCategoryController } from './controllers/category/CreateCategoryController';
 import { ListCategoryController } from './controllers/category/ListCategoryController';
 import { CreateProductController } from './controllers/product/CreateProductController';
+import { ListByCategoryController } from './controllers/product/ListByCategoryController';
 
 import { isAuthenticated } from './middlewares/isAuthenticated';
 import uploadConfig from './config/multer';
@@ -25,5 +26,6 @@ router.get('/category', isAuthenticated, new ListCategoryController().handle);
 
 // -- ROTAS PRODUCT -- //
 router.post('/product', isAuthenticated, uplaod.single('file'), new CreateProductController().handle);
+router.get('/category/product', isAuthenticated, new ListByCategoryController().handle);
 
 export { router };
