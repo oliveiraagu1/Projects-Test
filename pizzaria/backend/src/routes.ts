d35a6d916eb3a547a779148e0c9ad0b63d91b8cd
Gustavo <gustavofloripa98@gmail.com>
2022-03-13 20:49:05 -0300

    criando um pedido

--- pizzaria/backend/src/routes.ts
+++ pizzaria/backend/src/routes.ts
@@ -8,6 +8,7 @@ import { CreateCategoryController } from './controllers/category/CreateCategoryC
 import { ListCategoryController } from './controllers/category/ListCategoryController';
 import { CreateProductController } from './controllers/product/CreateProductController';
 import { ListByCategoryController } from './controllers/product/ListByCategoryController';
+import { CreateOrderController } from './controllers/order/CreateOrderController';
 
 import { isAuthenticated } from './middlewares/isAuthenticated';
 import uploadConfig from './config/multer';
@@ -27,5 +28,8 @@ router.get('/category', isAuthenticated, new ListCategoryController().handle);
 // -- ROTAS PRODUCT -- //
 router.post('/product', isAuthenticated, uplaod.single('file'), new CreateProductController().handle);
 router.get('/category/product', isAuthenticated, new ListByCategoryController().handle);
+
+// -- ROTAS ORDER -- //
+router.post('/order', isAuthenticated, new CreateOrderController().handle);
 
 export { router };
