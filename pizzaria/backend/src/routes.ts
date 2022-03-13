f09e94ce856357845aa120345ee2d3fc033e4df7
Gustavo <gustavofloripa98@gmail.com>
2022-03-12 23:33:58 -0300

    incluindo função cadastro categoria

--- pizzaria/backend/src/routes.ts
+++ pizzaria/backend/src/routes.ts
@@ -3,6 +3,7 @@ import { Router } from "express";
 import { CreateUserController } from './controllers/user/CreateUserController';
 import { AuthUserController } from './controllers/user/AuthUserController';
 import { DetailUserController } from './controllers/user/DetailUserController';
+import { CreateCategoryController } from "./controllers/category/CreateCategoryController";
 import { isAuthenticated } from './middlewares/isAuthenticated';
 
 const router = Router();
@@ -11,5 +12,8 @@ const router = Router();
 router.post('/users', new CreateUserController().handle);
 router.post('/session', new AuthUserController().handle);
 router.get('/me', isAuthenticated, new DetailUserController().handle);
+
+// -- ROTAS CATEGORY -- //
+router.post('/category', isAuthenticated, new CreateCategoryController().handle);
 
 export { router };
