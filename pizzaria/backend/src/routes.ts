a836625ac58b4409322c0ececc49043a9105d2ba
Gustavo <gustavofloripa98@gmail.com>
2022-03-13 19:44:47 -0300

    rotas produtos

--- pizzaria/backend/src/routes.ts
+++ pizzaria/backend/src/routes.ts
@@ -4,6 +4,7 @@ import { AuthUserController } from './controllers/user/AuthUserController';
 import { DetailUserController } from './controllers/user/DetailUserController';
 import { CreateCategoryController } from './controllers/category/CreateCategoryController';
 import { ListCategoryController } from './controllers/category/ListCategoryController';
+import { CreateProductController } from './controllers/product/CreateProductController';
 
 import { isAuthenticated } from './middlewares/isAuthenticated';
 const router = Router();
@@ -16,5 +17,8 @@ router.get('/me', isAuthenticated, new DetailUserController().handle);
 // -- ROTAS CATEGORY -- //
 router.post('/category', isAuthenticated, new CreateCategoryController().handle);
 router.get('/category', isAuthenticated, new ListCategoryController().handle);
+
+// -- ROTAS PRODUCT -- //
+router.post('/product', isAuthenticated, new CreateProductController().handle);
 
 export { router };
