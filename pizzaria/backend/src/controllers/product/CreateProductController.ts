63f544c9d6fd66bc1dcd1f073865991997b2f3e2
Gustavo <gustavofloripa98@gmail.com>
2022-03-13 20:05:17 -0300

    incluindo função multer

--- pizzaria/backend/src/controllers/product/CreateProductController.ts
+++ pizzaria/backend/src/controllers/product/CreateProductController.ts
@@ -5,16 +5,25 @@ class CreateProductController {
     async handle(req: Request, res: Response){
 
         const {name, price, description, category_id} = req.body;
-        let banner = '';
+
         const createProductService = new CreateProductService();
-        const product = await createProductService.execute({
-            name,
-            price,
-            description,
-            banner,
-            category_id
-        });
-        return res.json(product);
+
+        if(!req.file){
+            throw new Error("Error upload file!");
+        }else{
+            const { originalname, filename, } = req.file;
+            console.log(filename);
+
+            const product = await createProductService.execute({
+                name,
+                price,
+                description,
+                banner: '',
+                category_id
+            });
+            return res.json(product);
+        }
+
     }
 }
 
--- pizzaria/backend/src/routes.ts
+++ pizzaria/backend/src/routes.ts
@@ -1,4 +1,6 @@
 import { Router } from "express";
+import multer from "multer";
+
 import { CreateUserController } from './controllers/user/CreateUserController';
 import { AuthUserController } from './controllers/user/AuthUserController';
 import { DetailUserController } from './controllers/user/DetailUserController';
@@ -7,7 +9,10 @@ import { ListCategoryController } from './controllers/category/ListCategoryContr
 import { CreateProductController } from './controllers/product/CreateProductController';
 
 import { isAuthenticated } from './middlewares/isAuthenticated';
+import uploadConfig from './config/multer';
+
 const router = Router();
+const uplaod = multer(uploadConfig.upload("./tmp"));
 
 // -- ROTAS USER -- //
 router.post('/users', new CreateUserController().handle);
@@ -19,6 +24,6 @@ router.post('/category', isAuthenticated, new CreateCategoryController().handle)
 router.get('/category', isAuthenticated, new ListCategoryController().handle);
 
 // -- ROTAS PRODUCT -- //
-router.post('/product', isAuthenticated, new CreateProductController().handle);
+router.post('/product', isAuthenticated, uplaod.single('file'), new CreateProductController().handle);
 
 export { router };
