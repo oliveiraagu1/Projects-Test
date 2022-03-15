7fb1d703f86846d72415760ff5dba5f7a6918575
Gustavo <gustavofloripa98@gmail.com>
2022-03-15 00:44:05 -0300

    Criando tela login

--- pizzaria/frontend/src/pages/index.tsx
+++ pizzaria/frontend/src/pages/index.tsx
@@ -1,12 +1,29 @@
 import Head from 'next/head';
+import Image from 'next/image';
+import styles from '../../styles/home.module.scss';
+import logoImg from '../../public/logo.svg';
+import { Input } from '../components/ui/Input';
+
 export default function Home() {
   return (
     <>
         <Head>
             <title>Pizzaria - Faça seu login</title>
         </Head>
-        <div>
-            <h1>Sujeito Pizzaria</h1>
+        <div className={styles.containerCenter}>
+            <Image src={logoImg} alt="Logo pizzaria" />
+            <div className={styles.login}>
+                <form>
+                    <Input
+                        placeholder="Digite o seu e-mail"
+                        type="text"
+                    />
+                    <Input
+                        placeholder="Digite sua senha"
+                        type="password"
+                    />
+                </form>
+            </div>
         </div>
     </>
   )
