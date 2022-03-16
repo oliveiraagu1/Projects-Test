e45fb53ed1fde314cc0967621a197ff920af72fa
Gustavo <gustavofloripa98@gmail.com>
2022-03-16 01:36:09 -0300

    incluindo itens context

--- pizzaria/frontend/src/contexts/AuthContext.tsx
+++ pizzaria/frontend/src/contexts/AuthContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, ReactNode, useState} from 'react';
+import { createContext, ReactNode, useState, useEffect } from 'react';
 import { destroyCookie, setCookie, parseCookies } from 'nookies';
 import { api } from '../services/apiClient';
 import Router from 'next/router';
@@ -44,6 +44,24 @@ export function AuthProvider({ children }: AuthProviderProps){
 
     const [ user, setUser ] = useState<UserProps>();
     const isAuthenticated = !!user;
+
+    useEffect(() => {
+        // Tentar pegar algo no cookie
+        const { '@nextauth.token': token } = parseCookies();
+        if(token){
+            api.get('me').then(response => {
+                const { id, name, email } = response.data;
+                setUser({
+                    id,
+                    name,
+                    email
+                });
+            }).catch(() => {
+                // Se deu erro deslogar usuário.
+                signOut();
+            })
+        }
+    }, []);
 
     async function signIn({ email, password }: SignProps){
         try {
