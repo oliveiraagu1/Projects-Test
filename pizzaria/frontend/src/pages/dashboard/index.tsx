b1fe79244ff111337faeb827166129f2375a93a2
Gustavo <gustavofloripa98@gmail.com>
2022-03-19 18:17:13 -0300

    criando dashboard

--- pizzaria/frontend/src/pages/dashboard/index.tsx
+++ pizzaria/frontend/src/pages/dashboard/index.tsx
@@ -1,6 +1,12 @@
-import { canSSRAuth } from '../../utils/canSSRAuth';
 import Head from 'next/head';
+import styles from './styles.module.scss';
+import { canSSRAuth } from '../../utils/canSSRAuth';
 import { Header } from '../../components/Header';
+import { FiRefreshCcw } from 'react-icons/fi';
+
+
+
+
 
 export default function Dashboard(){
     return(
@@ -10,7 +16,24 @@ export default function Dashboard(){
             </Head>
             <div>
                 <Header/>
-                <h1>Painel</h1>
+                <main className={styles.container}>
+
+                    <div className={styles.containerHeader}>
+                        <h1>Últimos pedidos</h1>
+                        <button>
+                            <FiRefreshCcw size={25} color="#3FFFA3" />
+                        </button>
+                    </div>
+                    <article className={styles.listOrders}>
+                        <section className={styles.orderItem}>
+                            <button>
+                                <div className={styles.tag}/>
+                                <span>Mesa</span>
+                            </button>
+                        </section>
+                    </article>
+
+                </main>
             </div>
         </>
     )
