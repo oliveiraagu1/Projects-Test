7a8c41bd2186b13bf5debb7270f797fb804b97c5
Gustavo <gustavofloripa98@gmail.com>
2022-03-19 18:39:02 -0300

    Buscando pedidos

--- pizzaria/frontend/src/pages/dashboard/index.tsx
+++ pizzaria/frontend/src/pages/dashboard/index.tsx
@@ -1,14 +1,31 @@
+import { useState } from "react";
 import Head from 'next/head';
 import styles from './styles.module.scss';
 import { canSSRAuth } from '../../utils/canSSRAuth';
 import { Header } from '../../components/Header';
 import { FiRefreshCcw } from 'react-icons/fi';
+import { setupAPIClient } from '../../services/api';
+
+type OrderProps = {
+    id: string;
+    table: string | number;
+    status: boolean;
+    draft: boolean;
+    name: string | null;
+}
+interface HomeProps{
+    orders: OrderProps[];
+}
+
+export default function Dashboard({ orders }: HomeProps){
+
+    const [orderList, setOrderList] = useState(orders || []);
+
+    function handleOpenModalView(id: string){
+
+    }
 
 
-
-
-
-export default function Dashboard(){
     return(
         <>
             <Head>
@@ -25,12 +42,14 @@ export default function Dashboard(){
                         </button>
                     </div>
                     <article className={styles.listOrders}>
-                        <section className={styles.orderItem}>
-                            <button>
-                                <div className={styles.tag}/>
-                                <span>Mesa</span>
-                            </button>
-                        </section>
+                        {orderList.map( item => (
+                            <section key={item.id} className={styles.orderItem}>
+                                <button onClick={() => handleOpenModalView(item.id)}>
+                                    <div className={styles.tag}/>
+                                    <span>Mesa {item.table}</span>
+                                </button>
+                            </section>
+                        ))}
                     </article>
 
                 </main>
@@ -40,7 +59,12 @@ export default function Dashboard(){
 }
 
 export const getServerSideProps = canSSRAuth(async (context) => {
+
+    const apiClient = setupAPIClient(context);
+    const response = await apiClient.get('orders');
     return {
-        props: {}
+        props: {
+            orders: response.data
+        }
     }
 })
