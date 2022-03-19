9c8956c1b382780f8b668650932c44b885539aad
Gustavo <gustavofloripa98@gmail.com>
2022-03-19 19:55:21 -0300

    Estilizando modal

--- pizzaria/frontend/src/components/ModalOrder/index.tsx
+++ pizzaria/frontend/src/components/ModalOrder/index.tsx
@@ -1,5 +1,5 @@
 import Modal from 'react-modal';
-import styles from 'styles.module.scss';
+import styles from './styles.module.scss';
 import { FiX } from 'react-icons/fi';
 import { OrderItemProps } from '../../pages/dashboard';
 
@@ -43,6 +43,7 @@ export function ModalOrder({ isOpen, onRequestClose, order }:ModalOrderProps){
                <h2>Detalhes do pedido</h2>
                <span className={styles.table}>
                    Mesa: <strong>{order[0].order.table}</strong>
+
                </span>
                {order.map( item => (
                    <section key={item.id} className={styles.containerItem}>
@@ -50,6 +51,8 @@ export function ModalOrder({ isOpen, onRequestClose, order }:ModalOrderProps){
                        <span className={styles.description}>{item.product.description}</span>
                    </section>
                ))}
+
+               <button className={styles.buttonOrder} onClick={()=>{}}>Concluir pedido</button>
            </div>
        </Modal>
     )
