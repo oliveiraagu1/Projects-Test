28691087c49d14598c2ae865a87296932b8f8259
Gustavo <gustavofloripa98@gmail.com>
2022-03-21 22:12:05 -0300

    Iniciando mobile

---
+++ pizzaria/mobile/App.tsx
@@ -0,0 +1,13 @@
+import { View, StatusBar } from 'react-native';
+import SignIn from './src/pages/SignIn';
+
+export default function App() {
+  return (
+    <View>
+      <StatusBar backgroundColor="#1D1D2E" barStyle="light-content" translucent={false}/>
+      <SignIn/>
+    </View>
+  );
+}
+
+
---
+++ pizzaria/mobile/src/pages/SignIn/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+export default function SignIn(){
+    return(
+        <View>
+            <Text>Login</Text>
+        </View>
+    )
+}
