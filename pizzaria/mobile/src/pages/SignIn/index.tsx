6db338b1b86d63b388c5f561cced4c9e03e96718
Gustavo <gustavofloripa98@gmail.com>
2022-03-21 22:59:06 -0300

    tela de login

--- pizzaria/mobile/src/pages/SignIn/index.tsx
+++ pizzaria/mobile/src/pages/SignIn/index.tsx
@@ -1,9 +1,74 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import {
+    View,
+    Text,
+    StyleSheet,
+    Image,
+    TextInput,
+    TouchableOpacity
+} from 'react-native';
 export default function SignIn(){
     return(
-        <View>
-            <Text>Login</Text>
+        <View style={styles.container}>
+            <Image
+                style={styles.logo}
+                source={require('../../assets/logo.png')}
+            />
+            <View style={styles.inputContainer}>
+                <TextInput
+                    placeholder="Digite o seu e-mail"
+                    placeholderTextColor='#F0F0F0'
+                    style={styles.input}
+                />
+                <TextInput
+                    placeholder="Digite sua senha"
+                    placeholderTextColor='#F0F0F0'
+                    style={styles.input}
+                    secureTextEntry={true}
+                />
+                <TouchableOpacity style={styles.button}>
+                    <Text style={styles.buttonText}>Acessar</Text>
+                </TouchableOpacity>
+            </View>
         </View>
     )
 }
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#1D1D2E',
+    },
+    logo:{
+        marginBottom: 18
+    },
+    inputContainer:{
+        width: '95%',
+        alignItems: 'center',
+        justifyContent: 'center',
+        paddingVertical: 32,
+        paddingHorizontal: 14
+    },
+    input:{
+        width: '95%',
+        height: 40,
+        backgroundColor: '#101026',
+        marginBottom: 4,
+        paddingHorizontal: 8,
+        color: '#FFF',
+    },
+    button:{
+        width: '95%',
+        height: 40,
+        backgroundColor: '#3FFFA3',
+        borderRadius: 4,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    buttonText:{
+        fontSize: 18,
+        fontWeight: 'bold',
+        color: '#101026'
+    }
+})
--- pizzaria/mobile/src/routes/index.tsx
+++ pizzaria/mobile/src/routes/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, ActivityIndicator } from 'react-native';
 
 import AppRoutes from "./app.routes";
 import AuthRoutes from "./auth.routes";
@@ -7,6 +7,20 @@ import AuthRoutes from "./auth.routes";
 export default function Routes(){
     const isAuthenticated = false;
     const loading = false;
+
+    if(loading){
+        return(
+            <View style={{
+                flex: 1,
+                backgroundColor:"#1D1D2E",
+                justifyContent: "center",
+                alignItems:"center",
+            }}
+            >
+                <ActivityIndicator size={60} color="#F5F7FB"/>
+            </View>
+        )
+    }
 
    return(
         isAuthenticated ? <AppRoutes/> : <AuthRoutes/>
