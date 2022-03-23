433ed995bbf75f4b46346ac035750b7b185799d7
Gustavo <gustavofloripa98@gmail.com>
2022-03-23 10:14:10 -0300

    adicionando itens

--- pizzaria/mobile/src/pages/Order/index.tsx
+++ pizzaria/mobile/src/pages/Order/index.tsx
@@ -1,9 +1,18 @@
 import React, { useState, useEffect } from 'react';
-import {View, Text, StyleSheet, TouchableOpacity, TextInput, Modal} from "react-native";
+import {
+    View,
+    Text,
+    StyleSheet,
+    TouchableOpacity,
+    TextInput,
+    Modal,
+    FlatList
+} from "react-native";
 import { useRoute, RouteProp, useNavigation } from '@react-navigation/native';
 import { Feather } from '@expo/vector-icons';
 import { api } from "../../services/api";
 import { ModalPicker } from "../../components/ModalPicker";
+import { ListItem } from "../../components/ListItem";
 
 type RouteDetailParams = {
     Order:{
@@ -22,6 +31,13 @@ type ProductProps = {
   name:string;
 }
 
+type ItemProps = {
+    id: string;
+    product_id: string;
+    name: string;
+    amount: string | number;
+}
+
 type OrderRouteProps = RouteProp<RouteDetailParams, 'Order'>;
 
 export default function Order(){
@@ -30,12 +46,14 @@ export default function Order(){
 
     const [category, setCategory] = useState<CategoryProps[] | []>([]);
     const [categorySelected, setCategorySelected] = useState<CategoryProps | undefined >();
-    const [amount, setAmount] = useState('1');
     const [modalCategoryVisible, setModalCategoryVisible] = useState(false);
 
     const [products, setProducts] = useState<ProductProps[] | []>([]);
     const [productSelected, setProductSelected] = useState<ProductProps | undefined >();
     const [modalProductVisible, setModalProductVisible] = useState(false);
+
+    const [amount, setAmount] = useState('1');
+    const [items, setItems] = useState<ItemProps[] | []>([]);
 
     useEffect( () => {
         async function loadInfo(){
@@ -84,13 +102,32 @@ export default function Order(){
         setProductSelected(item);
     }
 
+    async function handleAdd(){
+        const response = await api.post('order/add', {
+            order_id: route.params.order_id,
+            product_id: productSelected?.id,
+            amount: Number(amount)
+        })
+
+        let data = {
+            id: response.data.id,
+            product_id: productSelected?.id as string,
+            name: productSelected?.name as string,
+            amount
+        }
+
+        setItems(oldArray => [...oldArray, data]);
+    }
+
     return(
         <View style={styles.container}>
             <View style={styles.header}>
                 <Text style={styles.title}>Mesa: {route.params.number}</Text>
-                <TouchableOpacity onPress={handleCloseOrder}>
-                    <Feather name="trash-2" size={28} color="#FF3F4B"/>
-                </TouchableOpacity>
+                {items.length === 0 && (
+                    <TouchableOpacity onPress={handleCloseOrder}>
+                        <Feather name="trash-2" size={28} color="#FF3F4B"/>
+                    </TouchableOpacity>
+                )}
             </View>
 
             {category.length !== 0 && (
@@ -123,13 +160,26 @@ export default function Order(){
             </View>
 
             <View style={styles.actions}>
-                <TouchableOpacity style={styles.buttonAdd}>
+                <TouchableOpacity style={styles.buttonAdd} onPress={handleAdd}>
                     <Text style={styles.buttonText}>+</Text>
                 </TouchableOpacity>
-                <TouchableOpacity style={styles.button}>
+
+                <TouchableOpacity
+                    style={[styles.button, { opacity: items.length === 0 ? 0.3 : 1}]}
+                    disabled={items.length === 0}
+                >
                     <Text style={styles.buttonText}>Avançar</Text>
                 </TouchableOpacity>
+
             </View>
+
+            <FlatList
+                data={items}
+                renderItem={ ({ item }) => <ListItem data={item} />}
+                keyExtractor={ item => item.id }
+                showsVerticalScrollIndicator={false}
+                style={{flex: 1, marginTop: 24}}
+            />
 
             <Modal
                 transparent={true}
