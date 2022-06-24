6bdd536a4560f1d18a74fed78e172a7df187a94f
Gustavo <gustavofloripa98@gmail.com>
2022-06-24 13:22:54 -0300

    subindo inclusão de cadastro no graphCMS

--- event-plataform/src/lib/apollo.ts
+++ event-plataform/src/lib/apollo.ts
@@ -1,6 +1,9 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-    uri: 'https://api-sa-east-1.graphcms.com/v2/cl4p3zxer1njr01z4dyeo1cv4/master',
+    uri: import.meta.env.VITE_API_URL,
+    headers: {
+        'Authorization': `Bearer ${import.meta.env.VITE_API_ACCESS_TOKEN}`
+    },
     cache: new InMemoryCache()
 })
