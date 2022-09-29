e834085952cfef4aa5ed67ce4d33c9c1ccb87217
Gustavo <gustavofloripa98@gmail.com>
2022-09-29 11:38:04 -0300

    finalizando auth user

--- barber/backend/src/services/user/DetailUserService.ts
+++ barber/backend/src/services/user/DetailUserService.ts
@@ -1,13 +1,32 @@
 import prismaClient from "../../prisma/index";
 
+interface UserProps{
+  user_id: string;
+}
+
 class DetailUserService {
-  async execute() {
+  async execute({ user_id }: UserProps) {
 
-  
+    const user = await prismaClient.user.findFirst({
+      where: {
+        id: user_id
+      },
+      select: {
+        id: true,
+        name: true,
+        email: true,
+        endereco: true,
+        subscription: {
+          select: {
+            id: true,
+            priceId: true,
+            status: true
+          }
+        }
+      }
+    })
 
-    return {
-
-    };
+    return user;
   }
 }
 
