4e364443f324432c193fce2c873f63c018919169
Gustavo Oliveira <gustavofloripa98@gmail.com>
2023-01-18 17:44:02 -0300

    criando estrutura do banco e rotas

--- habits/backend/src/server.ts
+++ habits/backend/src/server.ts
@@ -1,8 +1,10 @@
-import Fastify from "fastify"; 
-import cors from '@fastify/cors';
+import Fastify from "fastify";
+import cors from "@fastify/cors";
+import { appRoutes } from "./routes";
 
 const app = Fastify();
 app.register(cors);
+app.register(appRoutes);
 
 app
   .listen({
