a65522e0b7c3ba24f7c97b0ef9d820166e621892
Gustavo Oliveira <gustavofloripa98@gmail.com>
2023-08-10 17:21:27 -0300

    subindo melhorias no controller e use case

--- hero-tickets/backend/src/controllers/EventController.ts
+++ hero-tickets/backend/src/controllers/EventController.ts
@@ -9,7 +9,21 @@ class EventController {
     }
 
     async create(request: Request, response: Response, next: NextFunction) {
-        const eventData: Event = request.body;
+        let eventData: Event = request.body;
+        const files = request.files as any;
+
+        if (files) {
+            const banner = files.banner[0];
+            const flyers = files.flyers
+
+            eventData = {
+                ...eventData,
+                banner: banner.filename,
+                flyers: flyers.map((item: any) => item.filename),
+            }
+        }
+
+
         try {
             await this.eventUseCase.create(eventData)
             return response.status(201).json({ message: 'Evento criado com sucesso!' })
--- hero-tickets/backend/src/useCases/EventUseCase.ts
+++ hero-tickets/backend/src/useCases/EventUseCase.ts
@@ -1,13 +1,32 @@
 import { Event } from "../entities/Event";
 import { EventRepository } from "../repositories/EventRepository";
+import { HttpException } from '../interfaces/HttpException';
 
 class EventUseCase {
     constructor(private eventRepository: EventRepository) {
     }
 
     async create(eventData: Event) {
+
+        if (!eventData.banner) throw new HttpException(400, 'Banner is required')
+
+        if (!eventData.flyers) throw new HttpException(400, 'Flyers is required')
+
+        if (!eventData.location) throw new HttpException(400, 'Location is required')
+
+
+        const cityName = getCityNameByCoordinates(
+            eventData.location.latitude,
+            eventData.location.longitude
+        )
+
+
         const result = await this.eventRepository.add(eventData);
         return result;
+    }
+
+    private getCityNameByCoordinates(latitude, longitude){
+        
     }
 }
 
