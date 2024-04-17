417d0b9d28689e19457b0a0aeb655431753d28c6
Gustavo Oliveira <gustavofloripa98@gmail.com>
2024-04-17 12:45:31 -0300

    subindo files da primeira parte do web

---
+++ unite/web-react/src/components/header.tsx
@@ -0,0 +1,14 @@
+import logo from '../../public/vite.svg';
+
+export const Header = () => {
+    return (
+        <header className='flex items-center gap-5 py-2'>
+            <img src={logo} />
+
+            <nav className='flex items-center gap-5'>
+                <a href='' className='font-medium text-sm text-zinc-300'>Eventos</a>
+                <a href='' className='font-medium text-sm'>Participantes</a>
+            </nav>
+        </header>
+    )
+}
