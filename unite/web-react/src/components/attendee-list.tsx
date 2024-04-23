44f82ec7b8629ab1625e846369f66a32f034b403
Gustavo Oliveira <gustavofloripa98@gmail.com>
2024-04-23 13:55:33 -0300

    subindo files da ultima parte do projeto

--- unite/web-react/src/components/attendee-list.tsx
+++ unite/web-react/src/components/attendee-list.tsx
@@ -1,44 +1,105 @@
-import { ChangeEvent, useState } from 'react';
+import { ChangeEvent, useEffect, useState } from 'react';
 import { Search, MoreHorizontal, ChevronsLeft, ChevronLeft, ChevronsRight, ChevronRight } from 'lucide-react';
 import { IconButton } from './icon-button';
 import { Table } from './table/table';
 import { TableHeader } from './table/table-header';
 import { TableCell } from './table/table-cell';
 import { TableRow } from './table/table-row';
-import { attendees } from '../data/attendees';
+//import { attendees } from '../data/attendees';
 import dayjs from 'dayjs';
 import relativeTime from 'dayjs/plugin/relativeTime';
-import 'dayjs/locale/pt-br'
+import 'dayjs/locale/pt-br';
 
 dayjs.extend(relativeTime);
 dayjs.locale('pt-br');
 
+interface Attendee {
+    id: string;
+    name: string;
+    email: string;
+    createdAt: string;
+    checkedInAt: string | null;
+}
+
 export const AttendeeList = () => {
 
-    const [search, setSearch] = useState('');
-    const [page, setPage] = useState(1);
-    
-    const totalPages = Math.ceil(attendees.length / 10);
+    const [search, setSearch] = useState(() => {
+        const url = new URL(window.location.toString())
+
+        if (url.searchParams.has('search')) return url.searchParams.get('search') ?? '';
+
+        return '';
+    });
+
+    const [page, setPage] = useState(() => {
+        const url = new URL(window.location.toString());
+
+        if (url.searchParams.has('page')) return Number(url.searchParams.get('page'));
+
+        return 1;
+    });
+
+    const [total, setTotal] = useState(0);
+    const [attendees, setAttendees] = useState<Attendee[]>([]);
+
+    useEffect(() => {
+
+        const url = new URL('http://localhost:3333/events/ee56c95b-58a1-4c9a-a5e4-9ea59949e6a4/attendees');
+
+        url.searchParams.set('pageIndex', String(page - 1))
+        if (search.length > 0) url.searchParams.set('query', search)
+
+        fetch(url)
+            .then(response => response.json())
+            .then(data => {
+                setAttendees(data.attendees)
+                setTotal(data.total);
+            })
+    }, [page, search])
+
+
+    function setCurrentSearch(search: string) {
+        const url = new URL(window.location.toString());
+
+        url.searchParams.set('search', search);
+
+        window.history.pushState({}, "", url);
+
+        setSearch(search);
+    }
+
+    function setCurrentPage(page: number) {
+        const url = new URL(window.location.toString());
+
+        url.searchParams.set('page', String(page));
+
+        window.history.pushState({}, "", url);
+
+        setPage(page);
+    }
+
+    const totalPages = Math.ceil(total / 10);
 
     const onSearchInputChanged = (event: ChangeEvent<HTMLInputElement>) => {
-        setSearch(event.target.value);
+        setCurrentSearch(event.target.value);
+        setCurrentPage(1);
     }
 
     const goToFirstPage = () => {
-        setPage(1);
+        setCurrentPage(1);
     }
 
     const goToLastPage = () => {
-        setPage(totalPages);
+        setCurrentPage(totalPages);
     }
 
     const goToPreviousPage = () => {
-        setPage(page - 1);
+        setCurrentPage(page - 1);
     }
 
-
     const goToNextPage = () => {
-        setPage(page + 1);
+        setCurrentPage(page + 1);
+
     }
 
 
@@ -48,7 +109,12 @@ export const AttendeeList = () => {
                 <h1 className='text-2xl font-bold'>Participante</h1>
                 <div className="px-3 w-72 py-1.5 border border-white/10 rounded-lg flex items-center gap-3">
                     <Search className='size-4 text-emerald-300' />
-                    <input onChange={onSearchInputChanged} className="bg-transparent flex-1 outline-none border-0 p-0 text-sm" placeholder='Buscar participante' />
+                    <input
+                        onChange={onSearchInputChanged}
+                        value={search}
+                        className="bg-transparent flex-1 outline-none border-0 p-0 text-sm focus:ring-0"
+                        placeholder='Buscar participante...'
+                    />
                 </div>
             </div>
 
@@ -66,7 +132,7 @@ export const AttendeeList = () => {
                     </tr>
                 </thead>
                 <tbody>
-                    {attendees.slice((page - 1) * 10, page * 10).map((attendee) => (
+                    {attendees.map((attendee) => (
                         <TableRow key={attendee.id} className='border-b border-white/10 hover:bg-white/5'>
                             <TableCell>
                                 <input type='checkbox' className='size-4 bg-black/20 rounded border border-white/10' />
@@ -79,7 +145,11 @@ export const AttendeeList = () => {
                                 </div>
                             </TableCell>
                             <TableCell>{dayjs().to(attendee.createdAt)}</TableCell>
-                            <TableCell>{dayjs().to(attendee.checkedInAt)}</TableCell>
+                            <TableCell>
+                                {attendee.checkedInAt === null}
+                                ? <span className='text-zinc-400'>Não fez check-in</span>
+                                : dayjs().to(attendee.checkedInAt)
+                            </TableCell>
                             <TableCell>
                                 <IconButton transparent>
                                     <MoreHorizontal className='size-4' />
@@ -92,7 +162,7 @@ export const AttendeeList = () => {
                 <tfoot>
                     <tr>
                         <TableCell colSpan={3}>
-                            Monstrando 10 de {attendees.length} itens
+                            Monstrando {attendees.length} de {total} itens
                         </TableCell>
                         <TableCell className='text-right' colSpan={3}>
                             <div className='inline-flex items-center gap-8'>
@@ -122,5 +192,3 @@ export const AttendeeList = () => {
         </div>
     )
 }
-
-// 36:45
