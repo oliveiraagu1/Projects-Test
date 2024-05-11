c9bfc2b092a28b364bdf59e47835c5513d728c91
Gustavo Oliveira <gustavofloripa98@gmail.com>
2024-05-11 13:59:46 -0300

    feat initial header

---
+++ finance/app/(dashboard)/layout.tsx
@@ -0,0 +1,16 @@
+import { Header } from "@/components/header";
+
+type Props = {
+    children: React.ReactNode;
+}
+
+export default function DashboardLayout({ children }: Props) {
+    return (
+        <>
+            <Header />
+            <main className="px-3 lg:px-14">
+                {children}
+            </main>
+        </>
+    );
+}
