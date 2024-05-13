e1b8a923502f0fe119a6d8ae266ebd99b60ae4de
Gustavo Oliveira <gustavofloripa98@gmail.com>
2024-05-13 19:48:28 -0300

    feat: add new project mobile

---
+++ unite/mobile/src/app/index.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+export default function Home(){
+    return (
+        <View>
+            <Text>Hello reactr native</Text>
+        </View>
+    )
+}
+
+// 40:40
