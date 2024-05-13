3c7b8756256135d34f94e13a3b7d57c25b415a17
Gustavo Oliveira <gustavofloripa98@gmail.com>
2024-05-13 12:36:03 -0300

    feat:add post api

--- finance/app/(dashboard)/page.tsx
+++ finance/app/(dashboard)/page.tsx
@@ -1,8 +1,21 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { useNewAccount } from "@/features/accounts/hooks/use-new-account";
+
 
 export default function Home() {
+
+  const { isOpen, onOpen, onClose } = useNewAccount();
   return (
     <div>
       DashBoard Page
+
+      <Button
+        onClick={onOpen}
+      >
+        teste
+      </Button>
     </div>
   );
 }
--- finance/features/accounts/components/new-account-sheet.tsx
+++ finance/features/accounts/components/new-account-sheet.tsx
@@ -1,3 +1,5 @@
+import { insertAccountSchema } from '@/db/schema';
+import { z } from 'zod';
 import { useNewAccount } from '@/features/accounts/hooks/use-new-account';
 import {
     Sheet,
@@ -6,9 +8,27 @@ import {
     SheetHeader,
     SheetTitle
 } from '@/components/ui/sheet';
+import { AccountForm } from '@/features/accounts/components/account-form';
+import { useCreateAccount } from '@/features/accounts/api/use-create-account';
+
+const formSchema = insertAccountSchema.pick({
+    name: true
+});
+
+type FormValues = z.input<typeof formSchema>;
+
 
 export const NewAccountSheet = () => {
     const { isOpen, onOpen, onClose } = useNewAccount();
+    const mutation = useCreateAccount();
+
+    const onSubmit = (values: FormValues) => {
+        mutation.mutate(values, {
+            onSuccess: () => {
+                onClose();
+            }
+        });
+    }
 
     return (
         <Sheet open={isOpen} onOpenChange={onClose}>
@@ -21,6 +41,13 @@ export const NewAccountSheet = () => {
                         Create a new account to track your transactions.
                     </SheetDescription>
                 </SheetHeader>
+                <AccountForm
+                    onSubmit={onSubmit}
+                    disabled={mutation.isPending}
+                    defaultValues={{
+                        name: '',
+                    }}
+                />
             </SheetContent>
         </Sheet>
     )
