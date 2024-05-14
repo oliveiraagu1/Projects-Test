75762ef580ffef0c789a15730564014634f3dc22
Gustavo Oliveira <gustavofloripa98@gmail.com>
2024-05-14 18:12:45 -0300

    feat: add inital transactions form

---
+++ finance/features/transactions/hooks/use-new-transaction.ts
@@ -0,0 +1,13 @@
+import { create } from 'zustand';
+
+type newTransactionState = {
+    isOpen: boolean;
+    onOpen: () => void;
+    onClose: () => void;
+}
+
+export const useNewTransaction = create<newTransactionState>((set) => ({
+    isOpen: false,
+    onOpen: () => set({ isOpen: true }),
+    onClose: () => set({ isOpen: false })
+}));
