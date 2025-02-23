f52602d2fcd6d7d9c6633921e284d6805641ea76
Gustavo Oliveira <guoliveeira@icloud.com>
2025-02-22 21:19:08 -0300

    :sparkles: feat: Uploading project c-sharp

---
+++ connect/c-sharp/TechLibrary.Api/Infraestructure/DataAccess/TechLibraryDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TechLibrary.Api.Domain.Entities;
+
+namespace TechLibrary.Api.Infraestructure.DataAccess;
+
+public class TechLibraryDbContext : DbContext
+{
+    public DbSet<User> Users { get; set; }
+    public DbSet<Book> Books { get; set; }
+    public DbSet<Checkout> Checkouts { get; set; }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite("Data Source=D:\\Workspace\\TechLibraryDb.db");
+    }
+}
---
+++ connect/c-sharp/TechLibrary.Api/UseCases/Users/Register/RegisterUserUseCase.cs
@@ -0,0 +1,57 @@
+﻿using FluentValidation.Results;
+using TechLibrary.Api.Domain.Entities;
+using TechLibrary.Api.Infraestructure.DataAccess;
+using TechLibrary.Api.Infraestructure.Secutiry.Cryptography;
+using TechLibrary.Api.Infraestructure.Secutiry.Tokens.Access;
+using TechLibrary.Communication.Requests;
+using TechLibrary.Communication.Responses;
+using TechLibrary.Exception;
+
+namespace TechLibrary.Api.UseCases.Users.Register;
+
+public class RegisterUserUseCase
+{
+    public ResponseRegisteredUserJson Execute(RequestUserJson request)
+    {
+        var dbContext = new TechLibraryDbContext();
+
+        Validate(request, dbContext);
+
+        var cryptography = new BCryptAlgorithm();
+        var entity = new User
+        {
+            Email = request.Email,
+            Name = request.Name,
+            Password = cryptography.HashPassword(request.Password),
+        };
+
+        dbContext.Users.Add(entity);
+        dbContext.SaveChanges();
+
+        var tokenGenerator = new JwtTokenGenerator();
+
+        return new ResponseRegisteredUserJson
+        {
+            Name = entity.Name,
+            AccessToken = tokenGenerator.Generate(entity)
+        };
+    }
+
+    private void Validate(RequestUserJson request, TechLibraryDbContext dbContext)
+    {
+        var validator = new RegisterUserValidator();
+
+        var result = validator.Validate(request);
+
+        var existUserWithEmail = dbContext.Users.Any(user => user.Email.Equals(request.Email));
+        if (existUserWithEmail)
+            result.Errors.Add(new ValidationFailure("Email", "E-mail já registrado na plataforma!"));
+
+        if (result.IsValid == false) 
+        {
+               var errorMessages = result.Errors.Select(error => error.ErrorMessage).ToList();
+
+            throw new ErrorOnValidationException(errorMessages);
+        }
+    }
+}
